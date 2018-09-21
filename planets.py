dd9a3b305a3c9dfc8391177bd4f017ea7779990a
ethanbwaite <43476516+ethanbwaite@users.noreply.github.com>
2018-09-21 08:57:32 -0700

    Completion of Lab 0
    Converts Earth weight to Mars and Jupiter weight in pounds.

--- planets.py
+++ planets.py
@@ -1,6 +1,8 @@
 def weight_on_planets():
    # write your code here
    
+   earthWeight = float(input("How much do you weigh on Earth? "))
+   print("\nOn Mars you would weigh %g pounds. \nOn Jupiter you would weigh %g pounds." % ((earthWeight*0.38),(earthWeight*2.34)))
    
    
 if __name__ == '__main__':
